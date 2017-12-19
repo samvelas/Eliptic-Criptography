4d0e5dfcb414f654ef5c20052e7292c60064c27e
samvelas <samvel@asatryan.am>
2017-12-19 22:01:45 +0400

    Eliptic Criptography realization on C#

---
+++ Diskret/Point.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Diskret
+{
+    public class Point
+    {
+        public int x, y;
+        static int q = 12721, a = -5;
+        static int px = 9686;
+        static int py = 764;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // a mod 12721
+        public static int mod(int a)
+        {
+            return a % q < 0 ? a % q + q : a % q;
+        }
+
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return (p1.x == p2.x && p1.y == p2.y) ? true : false;
+        }
+
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return (p1.x == p2.x && p1.y == p2.y) ? false : true;
+        }
+
+        /// <summary>
+        /// Порядок точки
+        /// </summary>
+        /// <returns></returns>
+        public int n()
+        {
+            Point p2 = this;
+            //Нахождение порядка первой точки
+            for (int i = 2; ; i++)
+            {
+                Point p3 = p2 + this;
+
+                if (p3 == this)
+                {
+                    return i;
+                }
+
+                p2 = p3;
+            }
+        }
+
+        //Сложение точек
+        public static Point operator +(Point p1, Point p2)
+        {
+            if (p2 == new Point(-1, -1))
+            {
+                return p1;
+            }
+
+            if (p1 == -1 * p2)
+            {
+                return new Point(-1, 0);
+            }
+
+            int x, y, lambda = 0;
+
+            //Если равны, то lambda = (3 * (x1)^2 + a) / (2 * y1)
+            if (p1 == p2)
+            {
+                int up = mod(mod(p1.x * p1.x * 3) + a);
+                int down = mod(2 * p1.y);
+
+
+                while (true)
+                {
+                    if (mod(down * (lambda++)) == up)
+                    {
+                        lambda--;
+                        break;
+                    }
+
+                    if (lambda - 1 > q)
+                    {
+                        return p2;
+                    }
+                }
+
+            }
+
+            //Если точки разные, то lambda = (y2 - y1)/(x2 - x1)
+            else
+            {
+                int up = mod(p2.y - p1.y);
+                int down = mod(p2.x - p1.x);
+
+                while (true)
+                {
+                    if (mod(down * (lambda++)) == up)
+                    {
+                        lambda--;
+                        break;
+                    }
+
+                    if (lambda - 1 > q)
+                    {
+                        return new Point(px, py);
+                    }
+                }
+            }
+
+            // x = lambda ^ 2 - x1 - x2
+            x = mod(mod(lambda * lambda) - p1.x - p2.x);
+
+            // y = lambda * (x1 - x) - y1
+            y = mod(mod(lambda * mod(p1.x - x)) - p1.y);
+
+            return new Point(x, y);
+        }
+
+        /// <summary>
+        /// Умножение число на точку
+        /// </summary>
+        /// <param name="a">Число</param>
+        /// <param name="P">Точка</param>
+        /// <returns>а*Р</returns>
+        public static Point operator *(int a, Point P)
+        {
+
+            Point p1 = P;
+
+            //Для умножения числа а на точку Р нужно сложить точку Р с собой а раз 
+            for (int i = 0; i < Math.Abs(a) - 1; i++)
+            {
+                p1 = p1 + P;
+                
+
+                if (p1 == P)
+                {
+                    return new Point(-1, -1);
+                }
+            }
+
+            //Если а отрицательное число возвращает -р1
+            if (a < 0)
+            {
+                return new Point(-p1.x, - p1.y);
+            }
+
+            return p1;
+        }
+
+
+
+    }
+}
---
+++ Diskret/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Diskret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int px = 9686;
+            int py = 764;
+
+            //Точка P
+            Point P = new Point(px, py);
+
+            //Вторая точка
+            Point p2 = P;
+
+            //Массив первых 10 точек
+            Point[] Points = new Point[10];
+
+            //Точка P
+            Points[0] = P;
+
+            //Вторая точка
+            Points[1] = Points[0] + Points[0];
+
+            //Остальные точки
+            for(int i = 2; i <= 9; i++)
+            {
+                Points[i] = Points[i - 1] + Points[0];
+            }
+
+            Console.WriteLine("Первые десять точки, поражденные точкой P\n");
+
+            //Вывод точек
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Points[i].x + "\t" + Points[i].y);
+            }
+
+            Console.WriteLine("\nПорядок точки P(" + px + "," + py + ") равен " + P.n());
+
+
+            //а - произвольное число от 1 до порядка точки P
+            int a = 35;
+
+            //A - открытый ключ
+            Point A = a * P;
+            
+            //Сообщение Боба
+            Point M = Points[3] + Points[7];
+
+            //k - число от 0 до порядка точки P, выбранный Бобом
+            int k = 20;
+
+            //Точки B1 и B2, вычисленные Бобом
+            Point B1 = k * P;
+
+            Point B2 =  M + k * A;
+
+            //B2 - a * B1 = M + k * A - a * k * P = M + k * a * P - k * A * P = M
+
+            Point help = (-a) * B1 + k * A;
+
+            Point ans = help == new Point(-1, 0) ? M : null;
+       
+            Console.WriteLine("A\n" + A.x + " " + A.y + "\n");
+            Console.WriteLine("M\n" + M.x + " " + M.y + "\n");
+            Console.WriteLine("B1\n" + B1.x + " " + B1.y + "\n");
+            Console.WriteLine("B2\n" + B2.x + " " + B2.y + "\n");
+            Console.WriteLine("Ans\n" + ans.x + " " + ans.y + "\n");
+
+            Console.WriteLine("Нажмите на Enter для решения задачи волка и Бармалея или Esc для выхода");
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.Escape)
+            {
+                return;
+            }
+            else if(key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+            }
+
+            //----------------------Решение дополнительной задачи---------------------------------
+
+            //Секретный ключ волка
+            int va = 47;
+
+            //Открытый ключ
+            Point vA = va * P;
+
+            //Сообщение Бармалея
+            Point vM = Points[2] + Points[6];
+
+            //Случайное число, выбранное Бармалейем
+            int vk = 17;
+            
+            //В1 и В2, рассчитанные Бармалейем
+            Point vB1 = vk * P;
+            Point vB2 = vk * vA;
+
+            //Бармалей представляет сообщение в виде c1, c2, B1
+            int c1 = Point.mod(vB2.x * vM.x);
+            int c2 = Point.mod(vB2.y * vM.y);
+
+            //Чтобы расшивровать сообщение нужно с1 / ((a * vB1).x) и с2 / ((a * vB1).y)
+
+            Point vhelp = va * vB1;
+
+            //Расшифрованное сообщение 
+            Point vAns = new Point(0, 0);
+
+            //Решение уравнении a*B2.x*Ans.x = c1 mod 12721
+            while (true)
+            {
+                if (Point.mod(vhelp.x * (vAns.x++)) == c1)
+                {
+                    vAns.x--;
+                    break;
+                }
+            }
+
+            //Решение уравнении a*B2.y*Ans.y = c2 mod 12721
+            while (true)
+            {
+                if (Point.mod(vhelp.y * (vAns.y++)) == c2)
+                {
+                    vAns.y--;
+                    break;
+                }
+            }
+
+            Console.WriteLine("A\n" + vA.x + " " + vA.y + "\n");
+            Console.WriteLine("M\n" + vM.x + " " + vM.y + "\n");
+            Console.WriteLine("B1\n" + vB1.x + " " + vB1.y + "\n");
+            Console.WriteLine("B2\n" + vB2.x + " " + vB2.y + "\n");
+            Console.WriteLine("a * B1\n" + vhelp.x + " " + vhelp.y + "\n");
+            Console.WriteLine("c1\n" + c1 + "\n");
+            Console.WriteLine("c2\n" + c2 + "\n");
+            Console.WriteLine("Ans\n" + vAns.x + " " + vAns.y + "\n");
+
+
+        }
+    }
+}
